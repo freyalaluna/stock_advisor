0a86b74549b52240f4751cc23f790ad923ed090a
Freya LaLuna <freyalaluna@outlook.com>
2025-11-26 21:11:58 -0700

    Fixed sentiment grouping, and gave sentiment columns more descriptive names

--- joinStocksWithNews.py
+++ joinStocksWithNews.py
@@ -16,24 +16,26 @@ allHeadlines = (allHeadlines.select('*', sf.to_date(allHeadlines.timestamp))
                             .withColumnRenamed("to_date(timestamp)", "date"))
 
 #Left join stocks with headlines based on window starting date
-joined = (allStocks.join(allHeadlines, on=[allStocks.ticker == allHeadlines.ticker, allStocks.start_date == allHeadlines.date], how="left")
+joined = (allStocks.join(allHeadlines, on=[allStocks.ticker == allHeadlines.ticker, allStocks.end_date == allHeadlines.date], how="left")
                    .drop(allHeadlines.ticker)
                    .drop("date"))
 
 #Squash all duplicate rows, aggregating the headline scores that occurred on the same day
-squashed = (joined.groupBy(["ticker", "start_date"]).agg(sf.first("end_date").alias("end_date"),
+squashed = (joined.groupBy(["ticker", "end_date"]).agg(sf.first("start_date").alias("start_date"),
                                                         sf.first("window").alias("window"),
                                                         sf.first("seven_day_target").alias("seven_day_target"),
                                                         sf.collect_list("sentiment_label").alias("sentiment_label"),
                                                         sf.collect_list("sentiment_score").alias("sentiment_score"),))
 
 #Run a window over each ticker, aggregating the last 5 days/10 pieces of news within the partition as a new column
-timeframe = Window.partitionBy("ticker").orderBy("start_date").rowsBetween(-5,Window.currentRow)
+timeframe = Window.partitionBy("ticker").orderBy("end_date").rowsBetween(-5,Window.currentRow)
 stocksWithHeadlines = squashed.withColumns({"last_five_sentiments": sf.flatten(sf.collect_list(squashed.sentiment_label).over(timeframe)),
                                              "last_five_score": sf.flatten(sf.collect_list(squashed.sentiment_score).over(timeframe))})
 
-stocksWithHeadlines = (stocksWithHeadlines.withColumns({"last_five_sentiments": sf.slice(stocksWithHeadlines.last_five_sentiments, 1, 10),
-                                                       "last_five_score": sf.slice(stocksWithHeadlines.last_five_score, 1, 10)})
+stocksWithHeadlines = (stocksWithHeadlines.withColumns({"last_five_day_sentiments": sf.slice(stocksWithHeadlines.last_five_sentiments, 1, 10),
+                                                       "last_five_day_scores": sf.slice(stocksWithHeadlines.last_five_score, 1, 10)})
                                           .drop("sentiment_label", "sentiment_score"))
 
+stocksWithHeadlines = stocksWithHeadlines.select(["ticker", "start_date", "end_date", "window", "last_five_day_sentiments", "last_five_day_scores"])
+stocksWithHeadlines.filter(sf.size(stocksWithHeadlines.last_five_day_sentiments) > 0).show(100)
 stocksWithHeadlines.write.parquet("/StockAdvisor/datasets/filtered/stocksWithSentiments", mode="overwrite")
